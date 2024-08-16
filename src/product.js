d1e8648dac7b0cec5b705c97acc8020f0e6ca151
VivekKumar528 <vicky2820x@gmail.com>
2024-08-16 17:20:11 +0530

    16/08/2024-React Started

---
+++ src/product.js
@@ -0,0 +1,13 @@
+function product(){
+    return (
+        <div>
+        <h1> Welcome to React Class </h1>
+        
+            <ol>
+                <li>Python</li>
+            </ol>
+        </div>
+
+    )
+}
+export default product
